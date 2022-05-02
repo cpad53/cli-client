bd35093a53300768059548045499f99d0b7ccad0
Praveen G Shirali <praveengshirali@gmail.com>
2022-05-02 19:35:04 +0530

    Updated cli-client with functionality to push and pull events.

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
+import "fmt"
+import "bytes"
 import "os"
+import "strings"
+import "errors"
+import "time"
+import "encoding/json"
+import "net/http"
+import "io/ioutil"
+import "strconv"
+
 
 const HELP_TEXT = `
 _______________________________________________________________
@@ -30,9 +40,78 @@ _______________________________________________________________
 `
 
 
+type EventPayload struct {
+	Ts string `json:"ts"`
+	Msg string `json:"msg"`
+}
+
+
+
 func show_help () {
 	println(HELP_TEXT)
 	os.Exit(1)
+}
+
+
+func sendEvent(address string, args... string) (bool, error) {
+	if len(args) == 0 {
+		return false, errors.New("ERROR: No input received\n")
+	}
+	println("Sending ...")
+	event := &EventPayload{
+		Ts: strconv.FormatInt(time.Now().UnixMilli(), 10),
+		Msg: strings.Join(args, " "),
+	}
+	jMsg, err := json.Marshal(event)
+	if err == nil {
+		target := address + "/event"
+		jBytes := bytes.NewBuffer(jMsg)
+		resp, err := http.Post(target, "application/json", jBytes)
+		if err != nil {
+			return false, err
+		} else {
+			println("StatusCode:", resp.StatusCode)
+		}
+	} else {
+		return false, err
+	}
+	return true, nil
+}
+
+
+func getLastEvent(address string) (bool, error) {
+	target := address + "/lastEvent"
+	resp, err := http.Get(target)
+	if err != nil {
+		return false, err
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return false, err
+	}
+	// println(string(data))
+	var event EventPayload
+	err = json.Unmarshal(data, &event)
+	if err != nil {
+		return false, err
+	}
+	hr_suffix := ""
+	ts_int64, err := strconv.ParseInt(event.Ts, 10, 64)
+	if err == nil {
+		ts_time := time.UnixMilli(ts_int64)
+		hr_suffix = "( " + ts_time.Format(time.RFC850) + " )"
+	} else {
+		fmt.Println(err.Error())
+	}
+	fmt.Println("Timestamp:", event.Ts, hr_suffix)
+	fmt.Println("Event Msg:", event.Msg)
+	return true, nil
+}
+
+
+func showConfig() {
+	println("INSIGHT_SERVER Address Is:", os.Getenv("INSIGHT_SERVER"))
+	os.Exit(0)
 }
 
 
@@ -48,9 +127,17 @@ func main () {
 		}
 		switch os.Args[1] {
 		case "send":
-			println("Sending ...")
+			ok, err := sendEvent(address, os.Args[2:]...)
+			if !ok {
+				println(err.Error())
+				os.Exit(1)
+			}
 		case "last":
-			println("Getting ...")
+			ok, err := getLastEvent(address)
+			if !ok {
+				println(err.Error())
+				os.Exit(1)
+			}
 		}
 	}
 }
